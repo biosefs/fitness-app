cd2cd4968d3891a7653a2544d8a738699ef46187
biosefs <jaysonjaramillo010@gmail.com>
2024-12-15 16:35:02 +0800

    Add files via upload
    latest na integration

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,15 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
-
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
-
-module.exports = config;
+module.exports = {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      ['module:react-native-dotenv', {
+        moduleName: '@env',
+        path: '../.env', // Adjust the path to point to the root folder
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      }],
+      '@babel/plugin-transform-private-methods', // Add this line
+    ],
+  };
+  
